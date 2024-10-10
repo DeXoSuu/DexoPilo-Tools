5e8383b190161849c1ab3ae7ae4cf1b191845430
DeXoSuu <william.leib31@gmail.com>
2024-10-10 07:54:36 +0200

    Update Config.py

--- Settings/Program/Config/Config.py
+++ Settings/Program/Config/Config.py
@@ -1,9 +1,3 @@
-# Copyright (c) DexoPilo
-# See the file 'LICENSE' for copying permission
-# ----------------------------------------------------------------------------------------------------------------------------------------------------------|
-# EN: 
-#     - Do not touch or modify the code below. If there is an error, please contact the owner, but under no circumstances should you touch the code.
-#     - Do not resell this tool, do not credit it to yours.
 name_tool = "Dexo_Pilo"
 type_tool = "Multi-Tools"
 version_tool = "1.0"
@@ -13,9 +7,9 @@ creator = "DeXoSuu and Potopilo"
 platform = "Windows 10/11 & Linux"
 discord_server = "discord.gg/z25NeaqeEc"
 website = "dexopilo.shop"
-github_tool = "github.com/loxyteck/RedTiger-Tools"
-telegram = "t.me/redtigertools"
-url_config = "https://raw.githubusercontent.com/loxyteck/RedTiger-Tools/main/Settings/Program/Config/Config.py"
-url_downloads = "https://github.com/loxyteck/RedTiger-Tools/archive/main.zip"
-license = "https://github.com/loxyteck/RedTiger-Tools/blob/main/LICENSE"
+github_tool = "github.com/DeXoSuu/DexoPilo-Tools"
+telegram = "t.me/dexopilotools"
+url_config = "https://github.com/DeXoSuu/DexoPilo-Tools/blob/main/Settings/Program/Config/Config.py"
+url_downloads = "https://github.com/DeXoSuu/DexoPilo-Tools/archive/refs/tags/DexoPilo.zip"
+license = "https://github.com/DeXoSuu/DexoPilo-Tools/blob/main/LICENSE"
 copyright = "Copyright (c) DexoPilo 'LICENSE'"
