f1cebc1fe0dc16d7fc91fa48c6a353f4641ef2c9
DeXoSuu <william.leib31@gmail.com>
2024-10-10 20:34:36 +0200

    Update Site.py

--- Settings/Program/Site.py
+++ Settings/Program/Site.py
@@ -1,9 +1,3 @@
-# Copyright (c) DexoPilo
-# See the file 'LICENSE' for copying permission
-# ----------------------------------------------------------------------------------------------------------------------------------------------------------|
-# EN: 
-#     - Do not touch or modify the code below. If there is an error, please contact the owner, but under no circumstances should you touch the code.
-#     - Do not resell this tool, do not credit it to yours.
 from Config.Util import *
 from Config.Config import *
 try:
@@ -14,20 +8,12 @@ except Exception as e:
 Title("Site")
 
 print(f"""
-{white}[{red}01{white}] {red}->{white} Website
 {white}[{red}02{white}] {red}->{white} Discord
 {white}[{red}03{white}] {red}->{white} Github
-{white}[{red}04{white}] {red}->{white} Telegram
 """)
 
 try:
     choice = input(f"{BEFORE + current_time_hour() + AFTER} {INPUT} Site -> {color.RESET}")
-    if choice in ['1', '01']:
-        site = f"https://{website}"
-        webbrowser.open_new_tab(site)
-        print(f"{BEFORE + current_time_hour() + AFTER} {INFO} Access to the site \"{color.WHITE}{site}{color.RED}\"" + color.RESET)
-        Continue()
-        Reset()
     if choice in ['2', '02']:
         site = f"https://{discord_server}"
         webbrowser.open_new_tab(site)
@@ -37,13 +23,6 @@ try:
 
     if choice in ['3', '03']:
         site = f"https://{github_tool}"
-        webbrowser.open_new_tab(site)
-        print(f"{BEFORE + current_time_hour() + AFTER} {INFO} Access to the site \"{color.WHITE}{site}{color.RED}\"" + color.RESET)
-        Continue()
-        Reset()
-
-    if choice in ['4', '04']:
-        site = f"https://{telegram}"
         webbrowser.open_new_tab(site)
         print(f"{BEFORE + current_time_hour() + AFTER} {INFO} Access to the site \"{color.WHITE}{site}{color.RED}\"" + color.RESET)
         Continue()
